28476cdb290b16dca6510543116bedcebf31feae
GabrielVL <gavalo.99@gmail.com>
2020-05-28 12:26:43 -0600

    [Tested exercise 2] Tested addEvenMultOdd() method

--- src/com/itcr/ce/datos/datoslineales/Main.java
+++ src/com/itcr/ce/datos/datoslineales/Main.java
@@ -6,6 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        SinglyList list = new SinglyList();
+        for (int i = 0; i < 9; i++) {
+            list.add(i);
+        }
+        list.print();
+
+        list.addEvenMultodd().print();
+
     }
 
 }
