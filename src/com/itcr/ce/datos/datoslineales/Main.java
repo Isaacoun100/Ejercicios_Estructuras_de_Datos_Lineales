1ee292843a65fa5617922a8770a16e6acfcff17a
Isaac <Isaacoun100@estudiantec.cr>
2020-05-28 12:57:31 -0600

    [Testing] The main will display an organized view of the exercises

--- src/com/itcr/ce/datos/datoslineales/Main.java
+++ src/com/itcr/ce/datos/datoslineales/Main.java
@@ -1,10 +1,43 @@
 package com.itcr.ce.datos.datoslineales;
 
 import com.itcr.ce.datos.datoslineales.dataStructures.SinglyList;
+import import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        SinglyList newList = new SinglyList();
+        Random random = new Random();
+        int a=0;
+
+
+        while(a<15){
+            newList.add(random.nextInt(25));
+        }
+
+        System.out.println("The list is:");
+
+        newList.print();
+
+        System.out.println("Minimum and maximum are");
+
+        newList.maxmin();
+
+        System.out.println("Odd multiplied and even summed");
+
+        addEvenMultodd().print();
+
+        System.out.println("Each 2 nodes, will add the sum of the previous");
+
+        addPairOfNodes();
+        newList.print();
+
+        System.out.println("And their average");
+
+        newList.average();
+        newList.print();
+
 
     }
 
